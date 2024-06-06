ca16be4595ae9aa4d7a2d68eb3edc95c8e4745d9
bakirhammad <bakir.hammad@gmail.com>
2024-06-06 23:51:03 +0300

    Create server

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+// require("./models/db");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+// For any "Unassigned endpionts"
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  });
+  
