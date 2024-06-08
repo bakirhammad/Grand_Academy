4239bfd25ef1bf00291daa7a977a2a4b454cf165
bakirhammad <bakir.hammad@gmail.com>
2024-06-09 00:31:18 +0300

    createCourse Done

--- Frontend/src/components/CreateCourse/CreateCourse.js
+++ Frontend/src/components/CreateCourse/CreateCourse.js
@@ -1,14 +1,22 @@
 import React, { useContext, useState } from "react";
 import "./CreateCourse.css";
 import { userContext } from "../../App";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Loader from "../Extra/Loader";
 
 const CreateCourse = () => {
-  const { userInfo } = useContext(userContext);
+  const { userInfo, token } = useContext(userContext);
+  const navigate = useNavigate();
+
   const [courseTitle, setCourseTitle] = useState("");
-  const [courseBody, setBourseBody] = useState("");
+  const [courseBody, setCourseBody] = useState("");
   const [courseImage, setCourseImage] = useState("");
   const [courseDuration, setCourseDuration] = useState("");
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [loader, setloader] = useState(true);
+  const [showMessage, setshowMessage] = useState(false);
+  const [message, setMessage] = useState("");
 
   return (
     <div>
@@ -24,44 +32,139 @@ const CreateCourse = () => {
 
       {/* ===== Body part of createCourse page ===== */}
       <div className="mainCeateBody">
-      <div className="createCourseBody">
-        <div className="courseTitle">
-          <label>
-            The Course Title: <span className="star">*</span>
-          </label>
+        <div className="createCourseBody">
+          <div className="courseTitle">
+            <label>
+              The Course Title: <span className="star">*</span>
+            </label>
+            <br />
+            <input
+              className="createInputs"
+              placeholder="Course title"
+              onChange={(e) => {
+                setIsEmpty(false);
+                setCourseTitle(e.target.value);
+              }}
+            />
+          </div>{" "}
           <br />
-          <input className="createInputs" placeholder="Course title" />
-      
-        </div>    <br />
-
-        <div className="courseDuration">
-          <label>
-            Course Duration: <span className="star">*</span>
-          </label>
+          <div className="courseDuration">
+            <label>
+              Course Duration: <span className="star">*</span>
+            </label>
+            <br />
+            <input
+              type="number"
+              className="createInputs"
+              placeholder="Number of hourse ex: 120"
+              onChange={(e) => {
+                setIsEmpty(false);
+                setCourseDuration(e.target.value);
+              }}
+            />
+          </div>
           <br />
-          <input className="createInputs" placeholder="Number of hourse ex: 120" />
-        </div><br />
-
-        <div className="courseBody">
-          <label>
-            Course Description: <span className="star">*</span>
-          </label>
+          <div className="courseBody">
+            <label>
+              Course Description: <span className="star">*</span>
+            </label>
+            <br />
+            <textarea
+              className="createInputs"
+              rows={8}
+              placeholder="Course description"
+              onChange={(e) => {
+                setIsEmpty(false);
+                setCourseBody(e.target.value);
+              }}
+            />
+          </div>
           <br />
-          <textarea className="createInputs" rows={8} placeholder="Course description" />
-        </div><br />
-
-        <div className="courseImage">
-          <label>
-            Course Image URL: <span>(optional)</span>
-          </label>
+          <div className="courseImage">
+            <label>
+              Course Image URL: <span>(optional)</span>
+            </label>
+            <br />
+            <input
+              className="createInputs"
+              placeholder="Paste URT"
+              onChange={(e) => {
+                setCourseImage(e.target.value);
+              }}
+            />
+          </div>
           <br />
-          <input className="createInputs" placeholder="Paste URT" />
-        </div><br />
-
-        <button>Create The Course</button>
+          {isEmpty && (
+            <div style={{ color: "red" }}>Please fill all * fields</div>
+          )}
+          <button
+            className="createButton"
+            onClick={() => {
+              if (
+                courseBody === "" ||
+                courseDuration === "" ||
+                courseTitle === ""
+              ) {
+                return setIsEmpty(!isEmpty);
+              }
+              setshowMessage(true);
+              axios
+                .post(
+                  "http://localhost:5000/course/createCourse",
+                  {
+                    courseTitle,
+                    courseDuration,
+                    courseBody,
+                    courseImage,
+                  },
+                  {
+                    headers: { Authorization: `Bearer ${token}` },
+                  }
+                )
+                .then((result) => {
+                  setMessage(result.data.message);
+                  setIsEmpty(false);
+                  setloader(false);
+                })
+                .catch((err) => {
+                  setMessage(err.response.data.message);
+                  setIsEmpty(false);
+                  setloader(false);
+                });
+            }}
+          >
+            Create The Course
+          </button>
+        </div>
       </div>
-      </div>
-      {/* ===== End the Body ===== */}
+
+      {/* ===== Show Message and Loader part ===== */}
+      {showMessage && (
+        <div className="A">
+          <div className="B">
+            {loader ? (
+              <Loader />
+            ) : (
+              <>
+                {" "}
+                <div>{message}</div>
+                <button
+                  className="DoneButton"
+                  onClick={() => {
+                    setshowMessage(false);
+                    setloader(true);
+                    navigate("/home");
+                  }}
+                >
+                  Done
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      )}
+
+      {/* ================================== */}
     </div>
   );
 };
